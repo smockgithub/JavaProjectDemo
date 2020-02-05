1bdbe04d5c416226405df73a9f257e0580558c92
dongyu <smock130@msn.com>
2020-02-05 19:15:29 +0800

    add maven demo

---
+++ mavenDemo/src/main/java/com/smock/maven/dog/Dog.java
@@ -0,0 +1,7 @@
+package com.smock.maven.dog;
+
+public class Dog {
+    public void Show(){
+        System.out.println("dog:name");
+    }
+}
---
+++ mavenDemo/src/main/java/com/smock/maven/example/App.java
@@ -0,0 +1,19 @@
+package com.smock.maven.example;
+
+import com.smock.maven.dog.Dog;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+
+        Dog dog = new Dog();
+        dog.Show();
+    }
+
+}
